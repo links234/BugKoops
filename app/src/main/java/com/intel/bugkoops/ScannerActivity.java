f26ca7e537be9123e302cfa583f7279c8cf8d10d
Murtaza Alexandru <murtaza_alexandru73@yahoo.com>
2015-07-22 14:22:47 +0300

    Autoflash mode + save state

--- app/src/main/java/com/intel/bugkoops/ScannerActivity.java
+++ app/src/main/java/com/intel/bugkoops/ScannerActivity.java
@@ -1,11 +1,13 @@
 package com.intel.bugkoops;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.Display;
 import android.view.KeyEvent;
@@ -20,18 +22,29 @@ import com.google.zxing.ResultPoint;
 import com.journeyapps.barcodescanner.BarcodeCallback;
 import com.journeyapps.barcodescanner.BarcodeResult;
 import com.journeyapps.barcodescanner.CompoundBarcodeView;
+import com.journeyapps.barcodescanner.camera.CameraSettings;
 
 import java.util.List;
 
 public class ScannerActivity extends Activity implements CompoundBarcodeView.TorchListener {
     private static final String LOG_TAG = ScannerActivity.class.getSimpleName();
 
+    private static final String KEY_RELOAD_OPERATION = "reloadOperation";
+    private static final String KEY_FLASH_STATE = "mFlashState";
+    private static final String KEY_LOCKED_ORIENTATION = "mLockedOrientation";
+
     private static final int FLASH_STATE_OFF = 0;
-    private static final int FLASH_STATE_ON = 1;
+    private static final int FLASH_STATE_AUTO = 1;
+    private static final int FLASH_STATE_ON = 2;
+
+    private static final int RELOAD_TORCH_DELAY = 400;
+    private static final int RELOAD_NOTHING = 0;
+    private static final int RELOAD_FLASH_ON = 1;
+    private static final int RELOAD_FLASH_AUTO = 2;
 
     private CompoundBarcodeView mBarcodeScannerView;
     private Button mSwitchFlashButton;
-    private Button mSwitchOrientationLocking;
+    private Button mSwitchOrientationLockingButton;
 
     private int mFlashState;
     private int mLockedOrientation;
@@ -71,18 +84,87 @@ public class ScannerActivity extends Activity implements CompoundBarcodeView.Tor
         if (!hasFlash()) {
             mSwitchFlashButton.setVisibility(View.GONE);
         }
-        mFlashState = FLASH_STATE_OFF;
 
-        mSwitchOrientationLocking = (Button)findViewById(R.id.switch_orientation_locking);
+        mSwitchOrientationLockingButton = (Button)findViewById(R.id.switch_orientation_locking);
+
+        loadDefaultSettings();
+
+        if (savedInstanceState != null) {
+            mFlashState = savedInstanceState.getInt(KEY_FLASH_STATE);
+            mLockedOrientation = savedInstanceState.getInt(KEY_LOCKED_ORIENTATION);
+        }
+
+        Bundle bundle = getIntent().getExtras();
+        Log.d(LOG_TAG, "TEST1");
+        if (bundle != null) {
+            Log.d(LOG_TAG, "TEST2");
+            int reloadOperation = bundle.getInt(KEY_RELOAD_OPERATION);
+            switch (reloadOperation) {
+                case RELOAD_NOTHING:
+                    Log.d(LOG_TAG, "TEST3");
+                    break;
+                case RELOAD_FLASH_ON:
+                    mFlashState = FLASH_STATE_ON;
+                    final Handler handler = new Handler();
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            mBarcodeScannerView.setTorchOn();
+                        }
+                    }, RELOAD_TORCH_DELAY);
+                    Log.d(LOG_TAG, "FLASH_STATE_ON");
+                    break;
+                case RELOAD_FLASH_AUTO:
+                    Log.d(LOG_TAG, "TEST4");
+                    mFlashState = FLASH_STATE_AUTO;
+                    mBarcodeScannerView.getBarcodeView().getCameraSettings().setAutoTorchEnabled(true);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        updateLayout();
+    }
+
+    private void updateLayoutSwitchFlashlight() {
+        if (mFlashState == FLASH_STATE_OFF) {
+            mSwitchFlashButton.setText("FLASH OFF");
+        } else if (mFlashState == FLASH_STATE_AUTO) {
+            mSwitchFlashButton.setText("FLASH AUTO");
+        } else {
+            mSwitchFlashButton.setText("FLASH ON");
+        }
+    }
+
+    private void updateLayoutSwitchOrientationLocking() {
+        if (mLockedOrientation == ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED) {
+            mSwitchOrientationLockingButton.setText("UNLOCKED");
+        } else {
+            mSwitchOrientationLockingButton.setText("LOCKED");
+        }
+    }
+
+    private void updateLayout() {
+        updateLayoutSwitchFlashlight();
+        updateLayoutSwitchOrientationLocking();
+    }
+
+    private void loadDefaultSettings() {
+        mFlashState = FLASH_STATE_OFF;
         mLockedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED;
     }
 
     public void onSwitchFlashlight(View view) {
         if (mFlashState == FLASH_STATE_OFF) {
-            mBarcodeScannerView.setTorchOn();
+            reload(RELOAD_FLASH_AUTO);
+        } else if (mFlashState == FLASH_STATE_AUTO) {
+            reload(RELOAD_FLASH_ON);
         } else {
             mBarcodeScannerView.setTorchOff();
         }
+
+        updateLayoutSwitchFlashlight();
     }
 
     public void onSwitchOrientationLocking(View view) {
@@ -116,7 +198,8 @@ public class ScannerActivity extends Activity implements CompoundBarcodeView.Tor
             mLockedOrientation = orientation;
         }
         setRequestedOrientation(mLockedOrientation);
-        mSwitchOrientationLocking.setText("UNLOCK");
+
+        updateLayoutSwitchOrientationLocking();
     }
 
     private void unlockOrientation() {
@@ -124,7 +207,8 @@ public class ScannerActivity extends Activity implements CompoundBarcodeView.Tor
             mLockedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED;
             setRequestedOrientation(mLockedOrientation);
         }
-        mSwitchOrientationLocking.setText("LOCK");
+
+        updateLayoutSwitchOrientationLocking();
     }
 
     private boolean hasFlash() {
@@ -132,16 +216,38 @@ public class ScannerActivity extends Activity implements CompoundBarcodeView.Tor
                 .hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
     }
 
+    private void reload(int reloadOperation) {
+        Intent intent = getIntent();
+        overridePendingTransition(0, 0);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+
+        Bundle bundle = new Bundle();
+        bundle.putInt(KEY_RELOAD_OPERATION, reloadOperation);
+        intent.putExtras(bundle);
+
+        finish();
+
+        overridePendingTransition(0, 0);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onSaveInstanceState (Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(KEY_FLASH_STATE, mFlashState);
+        outState.putInt(KEY_LOCKED_ORIENTATION,mLockedOrientation);
+    }
+
     @Override
     public void onTorchOn() {
         mFlashState = FLASH_STATE_ON;
-        mSwitchFlashButton.setText("FLASH OFF");
+        updateLayoutSwitchFlashlight();
     }
 
     @Override
     public void onTorchOff() {
         mFlashState = FLASH_STATE_OFF;
-        mSwitchFlashButton.setText("FLASH ON");
+        updateLayoutSwitchFlashlight();
     }
 
     @Override
