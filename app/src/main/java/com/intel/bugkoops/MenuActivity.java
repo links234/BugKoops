6d0a886f5aa6c941430dd2b0a72644fddc087303
Murtaza Alexandru <murtaza_alexandru73@yahoo.com>
2015-07-23 13:55:47 +0300

    Added phyisical menu button shortcut to actionbar

--- app/src/main/java/com/intel/bugkoops/MenuActivity.java
+++ app/src/main/java/com/intel/bugkoops/MenuActivity.java
@@ -1,14 +1,20 @@
 package com.intel.bugkoops;
 
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MenuActivity extends ActionBarActivity {
     final String LOG_TAG = getClass().getSimpleName();
 
+    protected Menu mMenu;
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        mMenu = menu;
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
@@ -21,4 +27,12 @@ public class MenuActivity extends ActionBarActivity {
         }
     }
 
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_MENU) {
+            mMenu.performIdentifierAction(R.id.action_options, 0);
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
 }
