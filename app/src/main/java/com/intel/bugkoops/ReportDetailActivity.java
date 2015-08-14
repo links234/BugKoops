f4896423fb5b70486537cce4a4ec077a7683648a
Murtaza Alexandru <murtaza_alexandru73@yahoo.com>
2015-08-14 14:14:19 +0300

    Added report detail actionbar icons + layout

--- app/src/main/java/com/intel/bugkoops/ReportDetailActivity.java
+++ app/src/main/java/com/intel/bugkoops/ReportDetailActivity.java
@@ -1,6 +1,8 @@
 package com.intel.bugkoops;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class ReportDetailActivity extends MenuActivity {
 
@@ -19,6 +21,22 @@ public class ReportDetailActivity extends MenuActivity {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.detail_report_container, fragment)
                     .commit();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_report_detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 }
