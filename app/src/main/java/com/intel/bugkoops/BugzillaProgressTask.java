9f7238a6160937ed64578b9672b3f8ea7c50069d
Murtaza Alexandru <murtaza_alexandru73@yahoo.com>
2015-08-19 18:46:26 +0300

    Added login/logout for bugzilla + http/https requests + progress info

--- app/src/main/java/com/intel/bugkoops/BugzillaProgressTask.java
+++ app/src/main/java/com/intel/bugkoops/BugzillaProgressTask.java
@@ -2,9 +2,13 @@ package com.intel.bugkoops;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -14,31 +18,44 @@ import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
 
-public class BugzillaProgressTask extends AsyncTask<String, Void, Boolean> {
+import javax.net.ssl.HttpsURLConnection;
+
+public class BugzillaProgressTask extends AsyncTask<String, String, Boolean> {
     final String LOG_TAG = getClass().getSimpleName();
     final String USER_AGENT = "BugKoops";
 
     private ProgressDialog mDialog;
     private Activity mActivity;
 
-    private String mHttpResult;
+    private String mGetResult;
     private int mResponseCode;
 
     private String mTaskResult;
+
+    private String mServer;
+    private String mToken;
+
+    private String mUser;
+    private String mPassword;
 
     public BugzillaProgressTask(Activity activity) {
         mActivity = activity;
         mDialog = new ProgressDialog(activity);
 
-        mHttpResult = "";
+        mGetResult = "";
         mResponseCode = 0;
 
         mTaskResult = null;
+
+        mServer = "https://landfill.bugzilla.org/bugzilla-5.0-branch/";
+
+        mUser = "Here comes your default account login";
+        mPassword = "Here comes your default account password";
     }
 
     protected void onPreExecute() {
         mDialog.setTitle("Sending report to Bugzilla");
-        mDialog.setMessage("Authenticating ... ");
+        mDialog.setMessage("Logging in ... ");
         mDialog.show();
     }
 
@@ -61,12 +78,14 @@ public class BugzillaProgressTask extends AsyncTask<String, Void, Boolean> {
 
     protected Boolean doInBackground(final String... args) {
         try {
+            if(!login()) {
+                setTaskResult("Failed to login!");
+                return false;
+            }
 
-            String url = "http://www.google.com/";
-
-            if(httpGet(url)) {
-                Log.d(LOG_TAG, "Result http = " + mHttpResult);
-            } else {
+            publishProgress("Logging out ... ");
+            if(!logout()) {
+                setTaskResult("Failed to logout!");
                 return false;
             }
 
@@ -77,15 +96,104 @@ public class BugzillaProgressTask extends AsyncTask<String, Void, Boolean> {
         return false;
     }
 
-    private Boolean httpGet(String url) {
+    protected void onProgressUpdate(String... dialogText) {
+        mDialog.setMessage(dialogText[0]);
+    }
+
+    private boolean login() {
+        Uri builtUri = Uri.parse(mServer).buildUpon()
+                .appendPath("rest")
+                .appendPath("login")
+                .appendQueryParameter("login", mUser)
+                .appendQueryParameter("password", mPassword)
+                .build();
+
+        if(!get(builtUri.toString())) {
+            return false;
+        }
+
+        Log.d(LOG_TAG, "Result get = " + mGetResult);
+
+        return true;
+    }
+
+    private boolean logout() {
+        Uri builtUri = Uri.parse(mServer).buildUpon()
+                .appendPath("rest")
+                .appendPath("logout")
+                .appendQueryParameter("token", mToken)
+                .build();
+
+        if(!get(builtUri.toString())) {
+            return false;
+        }
+
+        Log.d(LOG_TAG, "Result get = " + mGetResult);
+
         try {
-            mHttpResult = "";
+            JSONObject json = new JSONObject(mGetResult);
+
+            if(errorResponse(json)) {
+                return false;
+            }
+        } catch(JSONException e) {
+            Log.e(LOG_TAG, "JSONException", e);
+            return false;
+        }
+
+        return true;
+    }
+
+    private boolean errorResponse(JSONObject json) {
+        try {
+            if(!json.has("error")) {
+                return false;
+            }
+            if(!json.getBoolean("error")) {
+                return false;
+            }
+            if(json.has("code")) {
+                Log.e(LOG_TAG, "Bugzilla error response with code: " + Integer.toString(
+                        json.getInt("code")));
+            }
+            if(json.has("message")) {
+                setTaskResult(json.getString("message"));
+            }
+        } catch(JSONException e) {
+            setTaskResult("Unexpected exception!");
+            return false;
+        }
+        return true;
+    }
+
+    private void setTaskResult(String result) {
+        if(mTaskResult == null) {
+            mTaskResult = result;
+        }
+    }
+
+    private boolean get(String url) {
+        return request(url, "GET");
+    }
+
+    private boolean request(String url, String method) {
+        Uri builtUri = Uri.parse(url);
+        if(builtUri.getScheme() == null || builtUri.getScheme().equals("https")) {
+            return httpsRequest(url, method);
+        } else {
+            return httpRequest(url, method);
+        }
+    }
+
+    private Boolean httpsRequest(String url, String method) {
+        try {
+            mGetResult = "";
             mResponseCode = 0;
 
             URL obj = new URL(url);
-            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+            HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
 
-            con.setRequestMethod("GET");
+            con.setRequestMethod(method);
             con.setRequestProperty("User-Agent", USER_AGENT);
 
             mResponseCode = con.getResponseCode();
@@ -100,7 +208,43 @@ public class BugzillaProgressTask extends AsyncTask<String, Void, Boolean> {
             }
             in.close();
 
-            mHttpResult = response.toString();
+            mGetResult = response.toString();
+
+            return true;
+        } catch(MalformedURLException e) {
+            Log.e(LOG_TAG, "MalformedInputException", e);
+        } catch(ProtocolException e) {
+            Log.e(LOG_TAG, "ProtocolException", e);
+        } catch(IOException e) {
+            Log.e(LOG_TAG, "IOException", e);
+        }
+        return false;
+    }
+
+    private Boolean httpRequest(String url, String method) {
+        try {
+            mGetResult = "";
+            mResponseCode = 0;
+
+            URL obj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+            con.setRequestMethod(method);
+            con.setRequestProperty("User-Agent", USER_AGENT);
+
+            mResponseCode = con.getResponseCode();
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            mGetResult = response.toString();
 
             return true;
         } catch(MalformedURLException e) {
