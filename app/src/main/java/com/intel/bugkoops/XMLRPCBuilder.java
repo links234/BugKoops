1ed8ec1cbfba689bf41dc5c42096ebd2380a6c83
Murtaza Alexandru <murtaza_alexandru73@yahoo.com>
2015-08-24 14:05:58 +0300

    Added XMLRPC helper class

---
+++ app/src/main/java/com/intel/bugkoops/XMLRPCBuilder.java
@@ -0,0 +1,90 @@
+package com.intel.bugkoops;
+
+import android.util.Xml;
+
+import org.xmlpull.v1.XmlSerializer;
+
+import java.io.StringWriter;
+
+public class XMLRPCBuilder {
+    final String LOG_TAG = getClass().getSimpleName();
+
+    private XmlSerializer mSerializer;
+    private StringWriter mWriter;
+
+    public XMLRPCBuilder() {
+        mSerializer = Xml.newSerializer();
+        mWriter = new StringWriter();
+    }
+
+    public void start(String methodName) {
+        mSerializer = Xml.newSerializer();
+        mWriter = new StringWriter();
+
+        try {
+            mSerializer.setOutput(mWriter);
+
+            mSerializer.startDocument("UTF-8", true);
+
+            mSerializer.startTag("", "methodCall");
+            mSerializer.startTag("", "methodName");
+            mSerializer.text(methodName);
+            mSerializer.endTag("", "methodName");
+
+            mSerializer.startTag("", "params");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void end() {
+        try {
+            mSerializer.endTag("", "params");
+
+            mSerializer.endTag("", "methodCall");
+
+            mSerializer.endDocument();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void startStruct() {
+        try {
+            mSerializer.startTag("", "param");
+            mSerializer.startTag("", "struct");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void endStruct() {
+        try {
+            mSerializer.endTag("", "struct");
+            mSerializer.endTag("", "param");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void member(String name, String value) {
+        try {
+            mSerializer.startTag("", "member");
+            mSerializer.startTag("", "name");
+            mSerializer.text(name);
+            mSerializer.endTag("", "name");
+            mSerializer.startTag("", "value");
+            mSerializer.startTag("", "string");
+            mSerializer.text(value);
+            mSerializer.endTag("", "string");
+            mSerializer.endTag("", "value");
+            mSerializer.endTag("", "member");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String toString() {
+        return mWriter.toString();
+    }
+}
