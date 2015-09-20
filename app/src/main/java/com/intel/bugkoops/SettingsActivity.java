456c406f480229dd72aec2b62a52bedef3ffcf45
Murtaza Alexandru <murtaza_alexandru73@yahoo.com>
2015-09-20 20:07:37 +0300

    Code refactor

--- app/src/main/java/com/intel/bugkoops/SettingsActivity.java
+++ app/src/main/java/com/intel/bugkoops/SettingsActivity.java
@@ -4,6 +4,7 @@ import android.annotation.TargetApi;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 
@@ -13,7 +14,44 @@ public class SettingsActivity extends PreferenceActivity {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.pref_general);
 
-        Preference resetDialogPreference = getPreferenceScreen().findPreference(getString(R.string.pref_resettodefault_key));
+        final ListPreference cameraIdListPreference = (ListPreference) findPreference(
+                getString(R.string.pref_cameraid_key));
+        cameraIdListPreference.setOnPreferenceChangeListener(
+                new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                cameraIdListPreference.setValue(newValue.toString());
+                preference.setSummary(cameraIdListPreference.getEntry());
+                return true;
+            }
+        });
+
+        final ListPreference flashStateListPreference = (ListPreference) findPreference(
+                getString(R.string.pref_flashstate_key));
+        flashStateListPreference.setOnPreferenceChangeListener(
+                new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                flashStateListPreference.setValue(newValue.toString());
+                preference.setSummary(flashStateListPreference.getEntry());
+                return true;
+            }
+        });
+
+        final ListPreference focusingModeListPreference = (ListPreference) findPreference(
+                getString(R.string.pref_focusingmode_key));
+        focusingModeListPreference.setOnPreferenceChangeListener(
+                new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                focusingModeListPreference.setValue(newValue.toString());
+                preference.setSummary(focusingModeListPreference.getEntry());
+                return true;
+            }
+        });
+
+        Preference resetDialogPreference = getPreferenceScreen().findPreference(
+                getString(R.string.pref_resettodefault_key));
 
         final Intent startIntent = getIntent();
 
